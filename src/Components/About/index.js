87aa8e411098443a681f4598267e0bde0fd768e2
Kevin Calle <kcalle012@gmail.com>
2025-01-19 22:30:45 -0500

    changed fonts

---
+++ src/Components/About/index.js
@@ -0,0 +1,14 @@
+import { useRef } from "react";
+import React from "react";
+import {motion, useInView} from "framer-motion";
+import "./index.scss";
+
+const About = () => {
+    return (
+        <div className="about">
+            
+        </div>
+    )
+};
+
+export default About;
