e4e6b0d2aac5b78c6abb73beca94e0c0036eb183
Kevin Calle <kcalle012@gmail.com>
2025-01-25 12:29:36 -0500

    fixed arm flyer and meeeting days

--- src/Components/Meetings/index.js
+++ src/Components/Meetings/index.js
@@ -9,7 +9,7 @@ const Meetings = () => {
             </h1>
             <div className="meetings-text">
             <p>
-                We typically meet on Thursdays from 4:00pm to 5:00pm biweekly, in CCIS
+                We typically meet on Thursdays from 4:00pm to 5:00pm weekly, in CCIS
                 236. Please pay attention to our <a target={"_blank"} rel="noreferrer" href="https://discord.com/invite/czpPCeNpah"> club discord</a> for
                 any changes in our schedule. We will also update this website to 
                 reflect the schedule; however, that is subject to change.
