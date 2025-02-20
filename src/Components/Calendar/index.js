74203bc9f062978d7c3edde9ecf10b77b4ccc438
Kevin Calle <kcalle012@gmail.com>
2025-02-20 16:25:19 -0500

    added react workshop

--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -37,54 +37,69 @@ const generateRecurringEvents = (startDate, recurrenceRule, totalOccurrences) =>
 
 const events = [
   {
-    title: 'Pizza Social',
+    title: "Pizza Social",
     start: new Date(2025, 1, 6, 16, 0),
     end: new Date(2025, 1, 6, 17, 0),
-    png: process.env.PUBLIC_URL + '/Images/Back.png', 
+    png: process.env.PUBLIC_URL + "/Images/Back.png",
   },
   {
-    title: 'Good Bear Speakers',
+    title: "Good Bear Speakers",
     start: new Date(2025, 1, 4, 15, 0),
     end: new Date(2025, 1, 4, 17, 0),
-    png: process.env.PUBLIC_URL + '/Images/goodbear-flyer.png', 
+    png: process.env.PUBLIC_URL + "/Images/goodbear-flyer.png",
   },
   {
-    title: 'ARM Workshop 1',
+    title: "Intro to React: Coding Your Personal Website",
+    start: new Date(2025, 1, 27, 15, 0),
+    end: new Date(2025, 1, 27, 17, 0),
+    png:
+      process.env.PUBLIC_URL +
+      "/Images/Intro-to-React-Crafting-Your-Personal-Website.png",
+  },
+  {
+    title: "Intro to React: Coding Your Personal Website",
+    start: new Date(2025, 2, 6, 15, 0),
+    end: new Date(2025, 2, 6, 17, 0),
+    png:
+      process.env.PUBLIC_URL + "/Images/Intro-to-React-Crafting-Your-Personal-Website.png",
+  },
+  {
+    title: "ARM Workshop 1",
     start: new Date(2025, 1, 6, 17, 30),
     end: new Date(2025, 1, 6, 19, 0),
-    png: process.env.PUBLIC_URL + '/Images/arm-flyer.png', 
+    png: process.env.PUBLIC_URL + "/Images/arm-flyer.png",
   },
   {
-    title: 'ARM Workshop 2',
+    title: "ARM Workshop 2",
     start: new Date(2025, 1, 20, 17, 30),
     end: new Date(2025, 1, 20, 19, 0),
-    png: process.env.PUBLIC_URL + '/Images/arm-flyer.png', 
+    png: process.env.PUBLIC_URL + "/Images/arm-flyer.png",
   },
   {
-    title: 'ARM Workshop 3',
+    title: "ARM Workshop 3",
     start: new Date(2025, 2, 6, 17, 30),
     end: new Date(2025, 2, 6, 19, 0),
-    png: process.env.PUBLIC_URL + '/Images/arm-flyer.png', 
+    png: process.env.PUBLIC_URL + "/Images/arm-flyer.png",
   },
   {
-    title: 'ARM Workshop 4',
+    title: "ARM Workshop 4",
     start: new Date(2025, 2, 20, 17, 30),
     end: new Date(2025, 2, 20, 19, 0),
-    png: process.env.PUBLIC_URL + '/Images/arm-flyer.png', 
+    png: process.env.PUBLIC_URL + "/Images/arm-flyer.png",
   },
   {
-    title: 'ARM Workshop 5',
+    title: "ARM Workshop 5",
     start: new Date(2025, 3, 3, 17, 30),
     end: new Date(2025, 3, 3, 19, 0),
-    png: process.env.PUBLIC_URL + '/Images/arm-flyer.png', 
+    png: process.env.PUBLIC_URL + "/Images/arm-flyer.png",
   },
   {
-    title: 'ARM Workshop 6',
+    title: "ARM Workshop 6",
     start: new Date(2025, 3, 17, 17, 30),
     end: new Date(2025, 3, 17, 19, 30),
-    png: process.env.PUBLIC_URL + '/Images/arm-flyer.png', 
+    png: process.env.PUBLIC_URL + "/Images/arm-flyer.png",
   },
-  ...generateRecurringEvents(new Date(2025, 1, 6, 16,0), 'weekly', 12) ,
+  ...generateRecurringEvents(new Date(2025, 1, 6, 16, 0), "weekly", 12),
 ];
 
 const MyCalendar = () => {
